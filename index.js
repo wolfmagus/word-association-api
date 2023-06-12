e7cd12132a4492586036602fb51f74dc1b8bcdeb
Aaron Bailey <wolfmagus0@gmail.com>
2023-06-11 20:28:58 -0400

    Server adjustments for mongodb atlas connection

--- index.js
+++ index.js
@@ -7,7 +7,33 @@ const app = express()
 app.use(cors())
 const path = require("path")
 
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://wolfmagus:M@ngekyou4464@cluster0.mdy4n.mongodb.net/word-association-app-database?retryWrites=true&w=majority";
+
+//Create a new MongoClient
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 app.use(express.static(path.join(__dirname, "client", "build")))
+
 
 app.get("/", (req,res)=> {
     res.json("hi")
